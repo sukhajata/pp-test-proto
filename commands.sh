1ed68e0ae342917d61c3880f51d5f1574e9640a7
Tim Cranfield <tcranfield@electronet.co.nz>
2020-03-05 11:22:11 +1300

    Added phase to message

--- commands.sh
+++ commands.sh
@@ -2,3 +2,8 @@
 export PATH=$PATH:~/go/bin
 
 protoc --go_out=plugins=grpc:. *.proto 
+
+#python
+pip install grpcio-tools
+
+python -m grpc_tools.protoc -I . --python_out=python --grpc_python_out=python *.proto
